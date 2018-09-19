ea97d5f35945d3b6ab0cce3ef2fdc039da712a36
Jason Connell <jconnell@delphicdigital.com>
2018-09-19 11:56:05 -0400

    Remove ps1 from git.

--- gentest.ps1
+++
@@ -1,3 +0,0 @@
-go build
-
-.\lpgagen -file datafiles\api_playerscorecardroundhole.txt -path "C:\Users\jconnell\source\repos\LPGALiveScoring\LPGAStatsService.Data" -obj StatsAPI_PlayerScorecardRoundHole -ns LPGAStatsService.Data
